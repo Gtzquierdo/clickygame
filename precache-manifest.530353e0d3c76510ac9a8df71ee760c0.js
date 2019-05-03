959c251c28136da9b1648097e163b3a2e04e87d7
Gtzquierdo <gtzquierdo@gmail.com>
2019-05-02 18:11:37 -0600

    Updates

---
+++ precache-manifest.530353e0d3c76510ac9a8df71ee760c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f13481990b9ea5debbd41082cb4210",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "065667c00d85ec41a0c1",
+    "url": "/clickygame/static/css/main.2db21102.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "065667c00d85ec41a0c1",
+    "url": "/clickygame/static/js/main.3b7172a2.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
